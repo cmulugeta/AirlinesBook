95d26cb3a47c66f1a16ad9df58c4b9f032dd8c1a
mulugeta c <cmulugeta.up@gmail.com>
2018-12-15 00:35:34 +0300

    feat: Adds a custom test runner to be used when integrating dagger with espresso for UI tests.

---
+++ app/src/androidTest/java/me/mfathy/airlinesbook/test/TestApplication.kt
@@ -0,0 +1,9 @@
+package me.cmulugeta.airlinesbook.test
+
+import android.app.Application
+
+/**
+ * Created by Mohammed Fathy on 15/12/2018.
+ * dev.cmulugeta@gmail.com
+ */
+class TestApplication : Application()
