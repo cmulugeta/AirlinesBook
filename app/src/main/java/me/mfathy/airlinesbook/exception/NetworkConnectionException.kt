bb39456520f3fb4abe6b01ce24da610666867ee6
mulugeta c <cmulugeta.up@gmail.com>
2018-12-22 23:05:49 +0300

    feat: Added exception handling to show suitable messages to the user.

---
+++ app/src/main/java/me/mfathy/airlinesbook/exception/NetworkConnectionException.kt
@@ -0,0 +1,9 @@
+package me.cmulugeta.airlinesbook.exception
+
+/**
+ * Created by Mohammed Fathy on 21/12/2018.
+ * dev.cmulugeta@gmail.com
+ *
+ * A custom network connection exception.
+ */
+class NetworkConnectionException : Exception()
---
+++ app/src/main/java/me/mfathy/airlinesbook/exception/NoFlightsException.kt
@@ -0,0 +1,9 @@
+package me.cmulugeta.airlinesbook.exception
+
+/**
+ * Created by Mohammed Fathy on 21/12/2018.
+ * dev.cmulugeta@gmail.com
+ *
+ * A custom exception to represent no flights result.
+ */
+class NoFlightsException : Exception()
