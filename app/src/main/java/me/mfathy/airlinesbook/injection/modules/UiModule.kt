cee1dd37cc628a13a526ec8ce433de4d05ce02ad
mulugeta c <cmulugeta.up@gmail.com>
2019-07-28 16:21:09 +0300

    refactor: refactored presentation layer to match domain layer implementation

--- app/src/main/java/me/mfathy/airlinesbook/injection/modules/UiModule.kt
+++ app/src/main/java/me/mfathy/airlinesbook/injection/modules/UiModule.kt
@@ -1,12 +1,7 @@
 package me.cmulugeta.airlinesbook.injection.modules
 
-import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
-import me.cmulugeta.airlinesbook.domain.executor.ExecutionThread
-import me.cmulugeta.airlinesbook.domain.executor.SubscribeThread
-import me.cmulugeta.airlinesbook.domain.executor.scheduler.IOThread
-import me.cmulugeta.airlinesbook.domain.executor.scheduler.UIThread
 import me.cmulugeta.airlinesbook.ui.details.FlightDetailsActivity
 import me.cmulugeta.airlinesbook.ui.search.SearchFlightsActivity
 import me.cmulugeta.airlinesbook.ui.select.SelectionActivity
@@ -17,11 +12,6 @@ import me.cmulugeta.airlinesbook.ui.select.SelectionActivity
 @Module
 abstract class UiModule {
 
-    @Binds
-    abstract fun bindPostExecutionThread(executionThread: UIThread): ExecutionThread
-
-    @Binds
-    abstract fun bindSubscribeThread(executionThread: IOThread): SubscribeThread
 
     @ContributesAndroidInjector
     abstract fun contributesSearchFlightsActivity(): SearchFlightsActivity
